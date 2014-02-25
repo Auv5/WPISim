769ab27a4bd7a0b31b9bc14e9f71deca9c98af5e
Jack McCracken <jackamc19@gmail.com>
2014-02-25 17:04:26 -0500

    Rewrite.

--- include/WPILib.h
+++
@@ -1,8 +0,0 @@
-#ifndef _WPILIB_H_
-#define _WPILIB_H_
-
-#include "SimpleRobot.hpp"
-
-#define START_ROBOT_CLASS(cls) int main () { SimpleRobot * rbt = new cls(); rbt->Main(); }
-
-#endif /* _WPILIB_H_ */
